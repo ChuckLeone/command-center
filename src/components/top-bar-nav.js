198e63328595a19c654676dcae7479b80291bab8
chuckleone <chuckleone@gmail.com>
2017-01-16 16:11:26 -0500

    - added touchtap event to user avatar
    - added badge to messages nav item in usermenu

--- src/components/top-bar-nav.js
+++ src/components/top-bar-nav.js
@@ -14,6 +14,9 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 const styles = {
+  avatar: {
+      margin: '0'
+  },
   title: {
     cursor: 'pointer',
   },
@@ -55,11 +58,18 @@ const styles = {
       title: {
           fontSize: '14px',
           backgroundColor: '#333333'
+      },
+      badges: {
+        color: 'white',
+        backgroundColor: 'red'
       }
   },
   icons: {
       avatar: {
           color: 'green'
+      },
+      messages: {
+          color: 'gold'
       },
       settings: {
           color: 'darkorchid'
@@ -70,7 +80,7 @@ const styles = {
       commandCenter: {
           color: 'white'
       }
-  },
+  }
 };
 
 class TopBar extends React.Component {
@@ -93,9 +103,10 @@ class TopBar extends React.Component {
             <AppBar
                 title={<span style={styles.title}>Organization Name</span>}
                 onLeftIconButtonTouchTap={this.handleToggle}
-                onRightIconButtonTouchTap={this.handleUserToggle}
+                onRightIconButtonTouchTap={this.handleToggle}
                 iconElementLeft={<IconButton><NavigationMenu /></IconButton>}
-                iconElementRight={<UserAvatar />}
+                iconElementRight={<IconButton><UserAvatar /></IconButton>}
+                iconStyleRight={styles.avatar}
                 style={{
                     backgroundColor: '#393939'
                 }} />
@@ -121,21 +132,21 @@ class TopBar extends React.Component {
                      <MenuItem onTouchTap={this.handleClose} leftIcon={<i className="fa fa-gear"></i>}><a href="#/settings" style={styles.drawer.MenuItem.a}>Settings</a></MenuItem>
                 </Drawer>
 
-                 <Drawer width={250} openSecondary={true} open={this.state.open} containerStyle={styles.userMenu.root}>
+                <Drawer width={250} openSecondary={true} open={this.state.open} containerStyle={styles.userMenu.root}>
                     <AppBar style={styles.userMenu.title} iconElementLeft={<FlatButton label="Bruce Wayne" style={styles.userMenu.avatar} />} iconElementRight={<UserAvatar style={styles.userMenu.avatar} />} />
                     <List style={styles.userMenu}>
-                        <ListItem onTouchTap={this.handleClose} primaryText="Messages" style={styles.userMenu} leftIcon={<i className="fa fa-envelope"></i>}><a href="#/" style={styles.drawer.MenuItem.a}></a></ListItem>
+                        <ListItem onTouchTap={this.handleClose} primaryText="Messages" style={styles.userMenu} rightIcon={<Badge badgeContent={4} badgeStyle={styles.userMenu.badges}/>} leftIcon={<i className="fa fa-envelope" style={styles.icons.messages}></i>}><a href="#/" style={styles.drawer.MenuItem.a}></a></ListItem>
                         <Divider style={styles.userMenu.hr} />
-                        <ListItem primaryText="My Profile" style={styles.userMenu} leftIcon={<i className="fa fa-user-circle" style={styles.icons.avatar}></i>} />
+                        <ListItem onTouchTap={this.handleClose} primaryText="My Profile" style={styles.userMenu} leftIcon={<i className="fa fa-user-circle" style={styles.icons.avatar}></i>} />
                         <Divider style={styles.userMenu.hr} />
-                        <ListItem primaryText="Settings" style={styles.userMenu} leftIcon={<i className="fa fa-gear" style={styles.icons.settings}></i>} />
+                        <ListItem onTouchTap={this.handleClose} primaryText="Settings" style={styles.userMenu} leftIcon={<i className="fa fa-gear" style={styles.icons.settings}></i>} />
                         <Divider style={styles.userMenu.hr} />
-                        <ListItem primaryText="Organization Home" style={styles.userMenu} leftIcon={<i className="fa fa-home" style={styles.icons.discovery}></i>} />
+                        <ListItem onTouchTap={this.handleClose} primaryText="Organization Home" style={styles.userMenu} leftIcon={<i className="fa fa-home" style={styles.icons.discovery}></i>} />
                         <Divider style={styles.userMenu.hr} />
-                        <ListItem primaryText="Command Center" style={styles.userMenu.active} leftIcon={<i className="fa fa-rocket" style={styles.icons.commandCenter}></i>} />
+                        <ListItem onTouchTap={this.handleClose} primaryText="Command Center" style={styles.userMenu.active} leftIcon={<i className="fa fa-rocket" style={styles.icons.commandCenter}></i>} />
                         <Divider style={styles.userMenu.hr} />
                     </List>
-                 </Drawer>
+                </Drawer>
             </div>
         );
     }
