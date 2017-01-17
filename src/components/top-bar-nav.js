eea05d37eef655cb6cfdea5efa3429fd26d2da6c
chuckleone <chuckleone@gmail.com>
2017-01-17 13:50:13 -0500

    - added activity stream to usermenu

--- src/components/top-bar-nav.js
+++ src/components/top-bar-nav.js
@@ -6,9 +6,11 @@ import MenuItem from 'material-ui/MenuItem';
 import Badge from 'material-ui/Badge';
 import {List, ListItem} from 'material-ui/List';
 import Divider from 'material-ui/Divider';
+import Subheader from 'material-ui/Subheader';
 import IconButton from 'material-ui/IconButton';
 import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
 import FlatButton from 'material-ui/FlatButton';
+import Avatar from 'material-ui/Avatar';
 import UserAvatar from './user-avatar';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
@@ -68,10 +70,7 @@ const styles = {
       },
       userCard: {
           backgroundColor: 'black',         
-          color: 'white',
-          secondaryText: {
-              color: 'white'
-          }
+          color: 'white'
       },
   },
   icons: {
@@ -93,6 +92,20 @@ const styles = {
   },
   secondaryText: {
       color: 'white'
+  },
+  Subheader: {
+      color: 'white',
+      textAlign: 'left',
+      fontSize: '18px',
+      backgroundColor: 'rgba(0, 0, 0, 0.2)'
+  },
+  ActivityFeed: {
+      position: 'absolute',
+      bottom: '0',
+
+  },
+  Avatar: {
+      backgroundColor: 'azure'
   }
 };
 
@@ -146,16 +159,14 @@ class TopBar extends React.Component {
                 </Drawer>
 
                 <Drawer width={300} openSecondary={true} open={this.state.open} containerStyle={styles.userMenu.root}>
-                    <AppBar style={styles.userMenu.title} iconElementLeft={<FlatButton label="Bruce W" style={styles.userMenu.avatar} />} iconElementRight={<UserAvatar style={styles.userMenu.avatar} />} />
+                    <AppBar style={styles.userMenu.title} iconElementLeft={<FlatButton style={styles.userMenu.avatar} />} iconElementRight={<UserAvatar style={styles.userMenu.avatar} />} />
                     <List style={styles.userMenu}>
                        <ListItem
-                        leftAvatar={<UserAvatar />}
                         rightIcon={<i className="fa fa-edit"></i>}
                         primaryText="Bruce Wayne"
                         secondaryText="bwayne@ublinked.edu"
                         style={styles.userMenu.userCard}
                         />
-                        <ListItem onTouchTap={this.handleClose} primaryText="Messages" style={styles.userMenu} rightIcon={<Badge badgeContent={4} badgeStyle={styles.userMenu.badges}/>} leftIcon={<i className="fa fa-envelope" style={styles.icons.messages}></i>}><a href="#/" style={styles.drawer.MenuItem.a}></a></ListItem>
                         <Divider style={styles.userMenu.hr} />
                         <ListItem onTouchTap={this.handleClose} primaryText="My Profile" style={styles.userMenu} leftIcon={<i className="fa fa-user-circle" style={styles.icons.avatar}></i>} />
                         <Divider style={styles.userMenu.hr} />
@@ -164,8 +175,17 @@ class TopBar extends React.Component {
                         <ListItem onTouchTap={this.handleClose} primaryText="Organization Home" style={styles.userMenu} leftIcon={<i className="fa fa-home" style={styles.icons.discovery}></i>} />
                         <Divider style={styles.userMenu.hr} />
                         <ListItem onTouchTap={this.handleClose} primaryText="Command Center" style={styles.userMenu.active} rightIcon={<Badge badgeContent={13} badgeStyle={styles.userMenu.badges}/>} leftIcon={<i className="fa fa-rocket" style={styles.icons.commandCenter}></i>} />
+                        </List>
                         <Divider style={styles.userMenu.hr} />
-                    </List>
+                        <List style={styles.ActivityFeed}>
+                        <Subheader style={styles.Subheader}>Activity Feed</Subheader>
+                        <ListItem onTouchTap={this.handleClose} primaryText="You submitted a request for a parking pass." leftIcon={<i className="fa fa-flag" />} style={styles.userMenu} />
+                        <ListItem onTouchTap={this.handleClose} primaryText="A new form approval request was generated." leftIcon={<i className="fa fa-flag" />} style={styles.userMenu} />
+                        <ListItem onTouchTap={this.handleClose} primaryText="A new event request was submitted." leftIcon={<i className="fa fa-flag" />} style={styles.userMenu} />
+                        <ListItem onTouchTap={this.handleClose} primaryText="Bake Sale form is awaiting your approval." leftIcon={<i className="fa fa-flag" />} style={styles.userMenu} />
+                        <Divider style={styles.userMenu.hr} />
+                         <ListItem onTouchTap={this.handleClose} primaryText="See more (15)" style={styles.userMenu} />
+                        </List>
                 </Drawer>
             </div>
         );
